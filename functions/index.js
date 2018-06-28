4d95914a6df3a15e9674938638070666066c05c0
venomousboxer <y.mohit03031999@gmail.com>
2018-06-28 17:27:04 +0530

    changes to implement notifications for ZALONIN

--- functions/index.js
+++ functions/index.js
@@ -2,31 +2,65 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+// var timeStamp = Date.now();
+// var bookingTimeStamp = timeStamp;
+// var folderTimeStamp = (bookingTimeStamp/1000000)|0;
+// console.log('booking : ' + bookingTimeStamp);
+// console.log('folder : ' + folderTimeStamp);
 
-exports.sendFollowersNotification = functions
-.database.ref('/messages').onWrite(event => {
+exports.sendMerchantNotificationsOnBooking = functions
+.database.ref('/MERCHANT/{salonName}/{folderTimeStamp}').onCreate((change, context) => {
+    const salonName = context.params.salonName;
     const payload = {
         notification : {
-            title : "Unread messages",
-            body : "New messages in the lobby"
-        },
-        topic : 'news'
+            title : "New Booking",
+            body : "New booking in zalonin"
+        }
     }
     return admin
         .database()
-        .ref('/messages')
+        .ref('/MERCHANT/' + salonName)
         .once('value')
         .then(data => {
-            console.log("Sending Notification to all devices")
-            return admin.messaging().send(payload);
+            console.log("Sending Notification to Merchant with saloon name : " + salonName);
+            if(data.val()){
+                return admin.messaging().sendToDevice(data.val().FCM_TOKEN, payload);
+            }
+            else{
+                console.log('unable to create snapshot of data');
+                return null;
+            }
         });
 });
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.stopSendingNotificationOnRejectionOfBooking = functions
+    .database.ref('/MERCHANT/{salonName}/{folderTimeStamp}').onDelete((change, context) => {
+        if(intervalId !== null){
+            clearInterval(intervalId);
+        }
+    });
+var intervalId = setInterval(sendNotificationAtRegularIntervals, 15000);
+function sendNotificationAtRegularIntervals() {
+    admin
+        .database()
+        .ref('/MERCHANT/' + salonName)
+        .once('value')
+        .then(data => {
+            console.log("Sending Notification to Merchant with saloon name : " + salonName);
+            if (data.val()) {
+                return admin.messaging().sendToDevice(data.val().FCM_TOKEN, payload);
+            }
+            else {
+                console.log('unable to create snapshot of data');
+                return null;
+            }
+        }).catch(console.error());
+}
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
 
 
 // exports.sendNotification = functions.database
